193074715d5065f91075ca5e88f1ba05f82fbe62
mercy-tarus <merciechelah1@gmail.com>
2024-04-02 11:40:36 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// code your solution here
+//sarturday activity
+function saturdayFun(activity = "roller-skate") 
+{
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // mondaywork function
+  const mondayWork = function(activity = "go to the office") 
+  {
+    return `This Monday, I will ${activity}.`;
+  }
+  
+  //wrapAdjective function 
+  function wrapAdjective(flair = "*") 
+  {
+    return function(adjective) 
+    {
+      return `You are ${flair}${adjective}${flair}!`;
+    }
+  }
